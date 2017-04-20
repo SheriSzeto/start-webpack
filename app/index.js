619b7951e01371ab3c1088fb04435f52b56e8228
situxin <13580370252@163.com>
2017-04-20 10:25:17 +0800

    修改两个简单的主文件

--- app/index.js
+++ app/index.js
@@ -1,3 +1,8 @@
 /**
  * Created by went on 2017-4-19.
  */
+var sub = require('./sub');
+var app = document.createElement('div');
+app.innerHTML = '<h1>Hello World</h1>';
+app.appendChild(sub());
+document.body.appendChild(app);
--- app/sub.js
+++ app/sub.js
@@ -1,3 +1,10 @@
 /**
  * Created by went on 2017-4-19.
  */
+function generateText(){
+    var element = document.createElement('h2');
+    element.innerHTML = "Hello h2 world";
+    return element;
+}
+
+module.exports = generateText;
